3acc0842b95699ced73a9665092b83bac200bb22
Vaia-Pasch <lemonpas@outlook.com>
2021-11-10 11:53:54 +0200

    made changes on all files(local)

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 getPrice = function() {
-    const numVal1 = Number(document.getElementById("price").value);
-    const numVal2 = Number(document.getElementById("count").value);
-    let totalValue = (numVal1 * numVal2) * 7;
+    const valueOfPrice = Number(document.getElementById("price").value);
+    const perWeek = Number(document.getElementById("count").value);
+    let totalValue = (valueOfPrice * perWeek) * 7;
     document.getElementById("total").value = totalValue.toFixed(0);
+}
+
+getPlastic = function() {
+    const amountOfCups = Number(document.getElementById("cup").value);
+    let getTotal = amountOfCups * 2;
+    document.getElementById("sum").value = getTotal.toFixed(0);
+}
+
+getStraw = function() {
+    const withStraw = Number(document.getElementById("straw").value);
+    let SumUp = withStraw * 3;
+    document.getElementById("sum").value = SumUp.toFixed(0);
 }
