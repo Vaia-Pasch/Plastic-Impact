63668db76f1b36b962376ea0cb29a133a361c538
Vaia-Pasch <lemonpas@outlook.com>
2021-11-15 21:20:43 +0200

    changes on styling

--- slide.js
+++ slide.js
@@ -17,5 +17,5 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "flex";  
 }
