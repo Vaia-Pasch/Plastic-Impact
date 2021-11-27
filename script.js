375d3dd98b3742fe9228690645db409645588444
Vaia-Pasch <lemonpas@outlook.com>
2021-11-27 16:01:29 +0200

    made changes on all files

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-getPrice = function () {
-    const valueOfPrice = Number(document.getElementById("price").value);
-    const perWeek = Number(document.getElementById("week").value);
-    // const perMonth = Number(document.getElementById("month").value);
-    // const perYear = Number(document.getElementById("year").value);
-    let weeklyValue = valueOfPrice * perWeek;
-    let monthlyValue = weeklyValue * 4;
-    let yearlyValue = weeklyValue * 52;
-    document.getElementById("total").value = weeklyValue.toFixed(2);
-    document.getElementById("month").value = monthlyValue.toFixed(2);
-    document.getElementById("year").value = yearlyValue.toFixed(2);
-}
-
-getPlastic = function () {
-    const amountOfCups = Number(document.getElementById("cup").value);
-    const amountOfStraws = Number(document.getElementById("straw").value);
-    let getTotalWeek = (amountOfCups * 2) + amountOfStraws;
-    let getTotalMonth = getTotalWeek * 4;
-    let getTotalYear = getTotalWeek * 52;
-    document.getElementById("sumOfWeek").value = getTotalWeek.toFixed(0);
-    document.getElementById("sumOfMonth").value = getTotalMonth.toFixed(0);
-    document.getElementById("sumOfYear").value = getTotalYear.toFixed(0);
-}
+calculate = function (type, firstInput, secondInput) {
+      const inputA = Number(document.getElementById(firstInput).value);
+      const inputB = Number(document.getElementById(secondInput).value);
+  
+    if (type === "bottle") {
+      let weeklyValue = inputA * inputB;
+      let monthlyValue = weeklyValue * 4;
+      let yearlyValue = weeklyValue * 52;
+  
+      document.getElementById("total").value = weeklyValue.toFixed(2);
+      document.getElementById("month").value = monthlyValue.toFixed(2);
+      document.getElementById("year").value = yearlyValue.toFixed(2);
+    }
+ 
+    if (type === "plastic") {
+        let getTotalWeek = inputA * 2 + inputB;
+        let getTotalMonth = getTotalWeek * 4;
+        let getTotalYear = getTotalWeek * 52;
+    
+        document.getElementById("sumOfWeek").value = getTotalWeek.toFixed(0);
+        document.getElementById("sumOfMonth").value = getTotalMonth.toFixed(0);
+        document.getElementById("sumOfYear").value = getTotalYear.toFixed(0);
+      }
+  };
