96ea9b1778fe09d1a90105830a9a17436ece375d
Vaia-Pasch <81173323+Vaia-Pasch@users.noreply.github.com>
2021-11-30 15:25:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
-calculate = function (type, firstInput, secondInput, straw) {
-      const inputA = Number(document.getElementById(firstInput).value);
-      const inputB = Number(document.getElementById(secondInput).value);
-      const strawOptionValue = straw && Number(document.getElementById(straw).value);
-  
-    if (type === "bottle") {
-      let weeklyValue = inputA * inputB;
-      let monthlyValue = weeklyValue * 4;
-      let yearlyValue = weeklyValue * 52;
-  
-      document.getElementById("total").value = weeklyValue.toFixed(2);
-      document.getElementById("month").value = monthlyValue.toFixed(2);
-      document.getElementById("year").value = yearlyValue.toFixed(2);
-    }
- 
-    if (type === "plastic") {
-        const straws = strawOptionValue ? inputB : 0;
-        let getTotalWeek = inputA * 2 + straws;
-        let getTotalMonth = getTotalWeek * 4;
-        let getTotalYear = getTotalWeek * 52;
-    
-        document.getElementById("sumOfWeek").value = getTotalWeek.toFixed(0);
-        document.getElementById("sumOfMonth").value = getTotalMonth.toFixed(0);
-        document.getElementById("sumOfYear").value = getTotalYear.toFixed(0);
-      }
-  };
+calculate = function (type, firstInput, secondInput) {
+  const inputA = Number(document.getElementById(firstInput).value);
+  const inputB = Number(document.getElementById(secondInput).value);
+
+  if (type === "bottle") {
+    let weeklyValue = inputA * inputB;
+    let monthlyValue = weeklyValue * 4;
+    let yearlyValue = weeklyValue * 52;
+
+    document.getElementById("total").value = weeklyValue.toFixed(2);
+    document.getElementById("month").value = monthlyValue.toFixed(2);
+    document.getElementById("year").value = yearlyValue.toFixed(2);
+  } else type === "plastic";
+  {
+    let getTotalWeek = inputA * 2 + inputB;
+    let getTotalMonth = getTotalWeek * 4;
+    let getTotalYear = getTotalWeek * 52;
+
+    document.getElementById("sumOfWeek").value = getTotalWeek.toFixed(0);
+    document.getElementById("sumOfMonth").value = getTotalMonth.toFixed(0);
+    document.getElementById("sumOfYear").value = getTotalYear.toFixed(0);
+  }
+};
 selection = function (option) {
   const value = option.value;
   const element = document.getElementById("counting");
